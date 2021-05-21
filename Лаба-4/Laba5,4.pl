58bfe2dc160e2f40f0e717103ba8fb60716db180
1qazio <63549716+1qazio@users.noreply.github.com>
2021-05-21 20:58:48 +0300

    Add files via upload

---
+++ Лаба-4/Laba5,4.pl
@@ -0,0 +1,2 @@
+readLNE:-write("N = "),read(N),nl,write("enter an elem"),nl,readList(N,List),write("position is "),nl,read(Number),listElNumb(List,Elem,Number),write("elem is "),write(Elem),!.
+readLNE:-write("is no elem").
