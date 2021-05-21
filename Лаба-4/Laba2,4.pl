533e6cc161d8cfc59cb55d8acffea50b8b8545c3
1qazio <63549716+1qazio@users.noreply.github.com>
2021-05-21 20:54:31 +0300

    Add files via upload

--- Лаба-4/Laba2,4.pl
+++ Лаба-4/Laba2,4.pl
@@ -1,3 +1,8 @@
 sumListDown(List,Sum):-sListD(List,0,Sum).
 sListD([],Sum,Sum):-!.
 sListD([H|L],CurSum,Sum):-CurSum1 is CurSum + H, sListD(L,CurSum1,Sum).
+
+readSumWriteList(N):-
+    readList(N,L),
+    sumListDown(L,Sum),
+    nl, write("Sum = "),write(Sum),nl,fail.
